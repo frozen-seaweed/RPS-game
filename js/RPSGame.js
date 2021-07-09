304dc035d04baf15832183ca43195be3cbf74a16
frozen-seaweed <kimdaun626@gmail.com>
2021-07-09 15:26:25 +0900

    start

---
+++ js/RPSGame.js
@@ -0,0 +1,52 @@
+    const imgs = document.getElementsByClassName('imgs');
+    const userBox = document.getElementById('userBox');
+    const compBox = document.getElementById('compBox');
+    const txtBox = document.getElementsByClassName('screen-txt');
+    let userChoice, compChoice;
+    let win = 0, loss = 0, draw = 0;
+    
+    $('.imgs').click( function() {
+        for (let i=0; i<txtBox.length; i++) {
+            txtBox[i].style.display = "none";
+        }
+
+        userBox.style.display = "block";
+        compBox.style.display = "block";
+
+        userBox.src = this.src;
+
+        compChoice = Math.round((Math.random() * 2) + 1);
+        switch (compChoice) {
+            case 1 : compBox.src = imgs[0].src; break;
+            case 2 : compBox.src = imgs[1].src; break;
+            case 3 : compBox.src = imgs[2].src; break;
+        }
+
+        for (let i=0; i<imgs.length; i++){
+            if (userBox.src === imgs[i].src) {
+                userChoice = i;
+                // break; 없어도 되는지
+            }
+        }
+
+        switch (userChoice + 1) {
+            case 1 : 
+                if (compChoice === 1) {draw++; break;}
+                if (compChoice === 2) {win++; break;}
+                if (compChoice === 3) {loss++; break;}
+            case 2 : 
+                if (compChoice === 1) {loss++; break;}
+                if (compChoice === 2) {draw++; break;}
+                if (compChoice === 3) {win++; break;}
+            case 3 : 
+                if (compChoice === 1) {win++; break;}
+                if (compChoice === 2) {loss++; break;}
+                if (compChoice === 3) {draw++; break;}
+        }
+
+        document.getElementById('win').innerHTML = win;
+        document.getElementById('loss').innerHTML = loss;
+        document.getElementById('draw').innerHTML = draw;
+
+        // return false; 없어도 되는지
+    });      
